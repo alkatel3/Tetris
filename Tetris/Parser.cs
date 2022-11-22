581ff4788cbbf39d812de116037804e3beda0b92
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-11-22 23:05:23 +0200

    Creat move for figure

---
+++ Tetris/Parser.cs
@@ -0,0 +1,25 @@
+﻿namespace Tetris
+{
+    public class Parser
+    {
+        public int Weight;
+        public int Height;
+        public char[,] Parse(string input)
+        {
+            var result = input.Split('\n');
+            Weight = result[1].Length;
+            Height = result.Length - 1;
+
+            char[,] p = new char[Height, Weight];
+
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Weight; j++)
+                {
+                    p[i, j] = result[i + 1][j];
+                }
+            }
+            return p;
+        }
+    }
+}
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -3,7 +3,15 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            var bl = new BL(new Parser());
+
+            bl.ReadFigure();
+            bl.Move();
+            bl.Show();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
